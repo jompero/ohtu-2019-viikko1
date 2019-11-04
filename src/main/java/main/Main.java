1387483642dceb2c3eb813a1484dd21a021a841e
jompero <dani.jompero@gmail.com>
2019-11-04 19:50:19 +0200

    Intentionally breaks style to test Code Climate

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,20 +4,34 @@ import ohtu.ohtuvarasto.*;
 
 public class Main {
 
+    static Varasto mehua = new Varasto(100.0);
+    static Varasto olutta = new Varasto(100.0, 20.2);
+
     public static void main(String[] args) {
+        alkuTilanne();
+        olutgetterit();
+        mehusetterit();
+        virhetilanteet();
+        lisaaOlutvarastoon(1000.0);
+        lisaaMehuvarastoon(-666.0);
+        otaOlutvarastosta(1000.0);
+        otaMehuvarastosta(-32.9);
+    }
 
-        Varasto mehua = new Varasto(100.0);
-        Varasto olutta = new Varasto(100.0, 20.2);
-
+    private static void alkuTilanne() {
         System.out.println("Luonnin jälkeen:");
         System.out.println("Mehuvarasto: " + mehua);
         System.out.println("Olutvarasto: " + olutta);
+    }
 
+    private static void olutgetterit() {
         System.out.println("Olutgetterit:");
         System.out.println("getSaldo()     = " + olutta.getSaldo());
         System.out.println("getTilavuus    = " + olutta.getTilavuus());
         System.out.println("paljonkoMahtuu = " + olutta.paljonkoMahtuu());
+    }
 
+    private static void mehusetterit() {
         System.out.println("Mehusetterit:");
         System.out.println("Lisätään 50.7");
         mehua.lisaaVarastoon(50.7);
@@ -25,7 +39,9 @@ public class Main {
         System.out.println("Otetaan 3.14");
         mehua.otaVarastosta(3.14);
         System.out.println("Mehuvarasto: " + mehua);
+    }
 
+    private static void virhetilanteet() {
         System.out.println("Virhetilanteita:");
         System.out.println("new Varasto(-100.0);");
         Varasto huono = new Varasto(-100.0);
@@ -34,27 +50,46 @@ public class Main {
         System.out.println("new Varasto(100.0, -50.7)");
         huono = new Varasto(100.0, -50.7);
         System.out.println(huono);
+    }
 
+    private static void olutvarastoTila() {
         System.out.println("Olutvarasto: " + olutta);
-        System.out.println("olutta.lisaaVarastoon(1000.0)");
-        olutta.lisaaVarastoon(1000.0);
-        System.out.println("Olutvarasto: " + olutta);
+    }
 
+    private static void mehuvarastoTila() {
         System.out.println("Mehuvarasto: " + mehua);
-        System.out.println("mehua.lisaaVarastoon(-666.0)");
-        mehua.lisaaVarastoon(-666.0);
-        System.out.println("Mehuvarasto: " + mehua);
+    }
 
-        System.out.println("Olutvarasto: " + olutta);
-        System.out.println("olutta.otaVarastosta(1000.0)");
-        double saatiin = olutta.otaVarastosta(1000.0);
-        System.out.println("saatiin " + saatiin);
-        System.out.println("Olutvarasto: " + olutta);
+    private static void lisaaOlutvarastoon(double amount) {
+        olutvarastoTila();
+        System.out.println(String.format("olutta.lisaaVarastoon(%f)", amount));
+        olutta.lisaaVarastoon(amount);
+        olutvarastoTila();
+    }
 
-        System.out.println("Mehuvarasto: " + mehua);
-        System.out.println("mehua.otaVarastosta(-32.9)");
-        saatiin = mehua.otaVarastosta(-32.9);
-        System.out.println("saatiin " + saatiin);
-        System.out.println("Mehuvarasto: " + mehua);
+    private static void lisaaMehuvarastoon(double amount) {
+        mehuvarastoTila();
+        System.out.println(String.format("mehua.lisaaVarastoon(%f)", amount));
+        mehua.lisaaVarastoon(amount);
+        mehuvarastoTila();
+    }
+
+    private static void otaOlutvarastosta(double amount) {
+        olutvarastoTila();
+        System.out.println(String.format("olutta.otaVarastosta(%f)", amount));
+        System.out.println("saatiin " + olutta.otaVarastosta(amount));
+        olutvarastoTila();
+    }
+
+    private static void otaMehuvarastosta(double amount) {
+        mehuvarastoTila();
+        System.out.println(String.format("mehua.otaVarastosta(%f)", amount));
+        System.out.println("saatiin " + mehua.otaVarastosta(amount));
+        mehuvarastoTila();
+    }
+
+    private static void breakingBad() 
+    {
+        System.out.println("lol");
     }
 }
--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -3,15 +3,14 @@ package ohtu.ohtuvarasto;
 public class Varasto {
 
     // --- piilotettu tietorakenteen toteutus: ---
-    private double tilavuus;  // paljonko varastoon mahtuu,  > 0
-    private double saldo;     // paljonko varastossa on nyt, >= 0
+    private double tilavuus = 0.0;  // paljonko varastoon mahtuu,  > 0
+    private double saldo = 0.0;     // paljonko varastossa on nyt, >= 0
 
     // --- konstruktorit: ---
     public Varasto(double tilavuus) {  // tilavuus on annettava
         if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
-        {
+        } else {
             this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
         saldo = 0;     // oletus: varasto on tyhjä
@@ -20,14 +19,8 @@ public class Varasto {
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
         if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
-        {
-            this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
-        if (alkuSaldo < 0.0) {
-            this.saldo = 0.0;
-        } else if (alkuSaldo <= tilavuus) // mahtuu
-        {
+        if (alkuSaldo <= tilavuus) {
             this.saldo = alkuSaldo;
         } else {
             this.saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
@@ -49,12 +42,10 @@ public class Varasto {
 
     // --- asettavat aksessorit eli setterit: ---
     public void lisaaVarastoon(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä 
-        {
+        if (maara < 0) {
             return;       // tällainen pikapoistuminenkin!
         }
-        if (maara <= paljonkoMahtuu()) // omia aksessoreita voi kutsua
-        {
+        if (maara <= paljonkoMahtuu()) {
             saldo = saldo + maara;          // ihan suoraan sellaisinaan
         } else {
             saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
@@ -62,8 +53,7 @@ public class Varasto {
     }
 
     public double otaVarastosta(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä 
-        {
+        if (maara < 0) {
             return 0.0;   // tällainen pikapoistuminenkin!
         }
         if (maara > saldo) {          // annetaan mitä voidaan
@@ -77,6 +67,7 @@ public class Varasto {
     }
 
     // --- Merkkijonoesitys Varasto-oliolle: ----
+    @Override
     public String toString() {
         return ("saldo = " + saldo + ", vielä tilaa " + paljonkoMahtuu());
     }
